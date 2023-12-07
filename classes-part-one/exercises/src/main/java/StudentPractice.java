188e431a4f939900261a5513f510e857ae75469d
StevenFenris <stevelarson87@gmail.com>
2023-12-07 14:00:24 -0600

    Update StudentPractice.java

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,7 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student newGuy = new Student("Steve", 12, 68, 3.7);
+        System.out.println(newGuy.studentInfo());
     }
 }

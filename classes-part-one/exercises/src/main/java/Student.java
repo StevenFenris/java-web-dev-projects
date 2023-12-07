3672890e244fc6c105c29d66a37de8567aba047f
StevenFenris <stevelarson87@gmail.com>
2023-12-07 13:59:51 -0600

    Uploaded from Work machine

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -3,8 +3,24 @@ public class Student {
         private int studentId;
         private int numberOfCredits = 0;
         private double gpa = 0.0;
+        public String getName(){
+                return name;
+        }
+        public void setName(String aName){
+                this.name = aName;
+        }
+        public Student(String name, int studentId, int numberOfCredits, double gpa) {
+                this.name = name;
+                this.studentId = studentId;
+                this.numberOfCredits = numberOfCredits;
+                this.gpa = gpa;
+        }
 
-        // Drop your getters and setters below for the Student class.
-        // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
+        public Student(String name, int studentId) {
+                this(name, studentId, 0, 0);
+        }
+        public String studentInfo() {
+                return (this.name + " has a GPA of: " + this.gpa);
+        }
 
 }
